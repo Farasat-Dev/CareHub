561721687f0b55c4e3cb48f6f7b4dfaf0da31dca
Farasat-Basra <chfrasatbasra@gmail.com>
2024-06-24 23:35:49 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './pages/Main.vue'
+</script>
+
+<template>
+  <MainPage />
+</template>
+
+<style scoped>
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NavigationBar from '../components/NavigationBar.vue'
+</script>
+;
+<template>
+  <div class="">
+   <NavigationBar /> 
+   <h1>this </h1>
+  </div>
+</template>
